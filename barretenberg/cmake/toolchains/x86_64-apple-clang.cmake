da089606893cfd1b06a2c1fcee965360270ad9cf
zac-williamson <blorktronics@gmail.com>
2022-01-11 16:50:49 +0000

    more m1 fix attempts

--- barretenberg/cmake/toolchains/x86_64-apple-clang.cmake
+++ barretenberg/cmake/toolchains/x86_64-apple-clang.cmake
@@ -1,2 +1,23 @@
 set(CMAKE_CXX_COMPILER "clang++")
 set(CMAKE_C_COMPILER "clang")
+
+if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm64")
+    set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+    set(cross_triple "aarch64-unknown-linux-gnu")
+    set(cross_root /usr/xcc/${cross_triple})
+
+    set(CMAKE_C_COMPILER $ENV{CC})
+    set(CMAKE_CXX_COMPILER $ENV{CXX})
+    set(CMAKE_Fortran_COMPILER $ENV{FC})
+
+    set(CMAKE_CXX_FLAGS "-I ${cross_root}/include/")
+
+    set(CMAKE_FIND_ROOT_PATH ${cross_root} ${cross_root}/${cross_triple})
+    set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+    set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+    set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+    set(CMAKE_SYSROOT ${cross_root}/${cross_triple}/sysroot)
+
+    set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-aarch64)
+endif()
