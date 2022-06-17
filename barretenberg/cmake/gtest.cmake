addf2e49fe1fd90f777c99aa171195c4bb68e3b1
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-06-17 11:55:50 +0100

    update gtest

--- barretenberg/cmake/gtest.cmake
+++ barretenberg/cmake/gtest.cmake
@@ -5,10 +5,11 @@ if(TESTING)
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG release-1.8.0
+        GIT_TAG release-1.10.0
     )
 
     FetchContent_GetProperties(googletest)
+
     if(NOT googletest_POPULATED)
         FetchContent_Populate(googletest)
         add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
